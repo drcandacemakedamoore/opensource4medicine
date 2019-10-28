eda8aa8aaa58f8d142ee642cdf989d3b5ad45db4
Candace Makeda Moore, MD <31033601+drcandacemakedamoore@users.noreply.github.com>
2019-10-28 18:19:08 +0200

    Update augmentedimages.py

--- radiopaedia/notebooks/augmentedimages.py
+++ radiopaedia/notebooks/augmentedimages.py
@@ -1,26 +1,39 @@
-import os
 
-from PIL import Image
+
+import os
+import numpy as np
+#import cv2
+from PIL import Image, ImageFilter
 
 #taking pictures of my harddrive, must be changed soon
-pictures = os.listdir (r"C:\Users\makeda\programming\catapop\images")
+pictures = os.listdir (r"C:\Users\makeda\programming\catapop\pimages")
+
+
+
 
 file_currently_reading = 0
 file_number_count = len(pictures)
+
  
 while file_currently_reading < file_number_count:
     filename = pictures[file_currently_reading]
 
-    im = Image.open(r"C:\Users\makeda\programming\catapop\images\\" + filename)
-  ## do some basic matrix math
+    im = Image.open(r"C:\Users\makeda\programming\catapop\pimages\\" + filename)
+  ## do some basic matrix math to scew it
     im= im.transform(im.size, Image.AFFINE, [1,-0.0018, -20,0.039,1.0337,-20],resample=Image.BICUBIC)
-    
-    ## add noise- add a nother line of code for that below
-    ##-#pending, must check PIL documentation
-    
-    
-    ##saveoutput
+  ## add a gaussian filter -add noise based on a standard deviation + mean *needs a better explanation
+    im_gauss= im.filter(ImageFilter.GaussianBlur(1))
+##boxblur- add an explantion
+    im_otherfilter =im.filter(ImageFilter.BoxBlur(3))
+
     im.save(r"C:\Users\makeda\programming\catapop\images\transformed\\"+filename,"PNG")
-        
+  #  make_it_noisy(im) = im_gauss
+    im_gauss.save(r"C:\Users\makeda\programming\catapop\images\transformedgaussian\\"+filename,"PNG")
+    im_otherfilter.save(r"C:\Users\makeda\programming\catapop\images\otherfilter\\"+filename,"PNG")    
     file_currently_reading =    file_currently_reading +1
-    
+
+
+
+
+
+
